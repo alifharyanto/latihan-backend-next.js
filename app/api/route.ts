109ff3368ed2b6f6f6abef3401713ddebc2f1806
Muhammad Alif Haryanto <alifharyanto201012@gmail.com>
2025-11-14 09:43:59 +0700

    update route.js api

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,13 @@
 export async function GET() {
-  return Response.json({
-    message: "Halo aku dari backend next js"
-  });
+  return new Response(
+    JSON.stringify({ message: "Halo aku dari backend next js" }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "https://superlative-pony-43d0be.netlify.app/",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type"
+      }
+    }
+  );
 }
